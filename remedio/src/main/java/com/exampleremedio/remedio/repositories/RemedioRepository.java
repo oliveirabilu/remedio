7ed457c33661877b6c06914dbaf2452f1986e7ca
oliveirabilu <carloscesaroliveira13@hotmail.com>
2025-11-14 11:07:09 -0300

    feature: Criar nova busca por ativo true

--- remedio/src/main/java/com/exampleremedio/remedio/repositories/RemedioRepository.java
+++ remedio/src/main/java/com/exampleremedio/remedio/repositories/RemedioRepository.java
@@ -3,5 +3,9 @@ package com.exampleremedio.remedio.repositories;
 import com.exampleremedio.remedio.entities.Remedio;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Arrays;
+import java.util.List;
+
 public interface RemedioRepository extends JpaRepository<Remedio, Long> {
+    List<Remedio> findAllByAtivoTrue();
 }
