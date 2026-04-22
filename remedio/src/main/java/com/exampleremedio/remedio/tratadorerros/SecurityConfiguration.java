a8a6977a87c13409a225672a8ebc25297a2c2a40
oliveirabilu <carloscesaroliveira13@hotmail.com>
2026-04-22 15:30:19 -0300

    feature: Criado uma classe para fazermos a configuração de segurança

---
+++ remedio/src/main/java/com/exampleremedio/remedio/tratadorerros/SecurityConfiguration.java
@@ -0,0 +1,9 @@
+package com.exampleremedio.remedio.tratadorerros;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {//será responsável pela configuração de segurança
+}
