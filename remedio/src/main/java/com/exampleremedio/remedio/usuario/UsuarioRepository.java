7d116b3c7d039f157364e9e6370a8a31c7456ac0
oliveirabilu <carloscesaroliveira13@hotmail.com>
2026-04-22 15:18:17 -0300

    feature: Criado uma interface repositorio Usuario, mapeando a (Entity e Id), e agora foi colocado mais um metodo customizado findByLogin, que irá ser usado para identificar o usuario

--- remedio/src/main/java/com/exampleremedio/remedio/usuario/UsuarioRepository.java
+++ remedio/src/main/java/com/exampleremedio/remedio/usuario/UsuarioRepository.java
@@ -1,6 +1,8 @@
 package com.exampleremedio.remedio.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByLogin(String login);
 }
