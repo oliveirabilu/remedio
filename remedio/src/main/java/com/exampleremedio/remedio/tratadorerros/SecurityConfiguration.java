140f90c10336200557e62debc91548f38076541f
oliveirabilu <carloscesaroliveira13@hotmail.com>
2026-04-22 16:03:30 -0300

    feature: Criado um bean SecurityFilterChain, que terá como parametros HttpSecurity, e desabilitamos o csrf, já que será uma Rest e criamos a politica de sessão sem manter estado no banco

--- remedio/src/main/java/com/exampleremedio/remedio/tratadorerros/SecurityConfiguration.java
+++ remedio/src/main/java/com/exampleremedio/remedio/tratadorerros/SecurityConfiguration.java
@@ -1,9 +1,22 @@
 package com.exampleremedio.remedio.tratadorerros;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {//será responsável pela configuração de segurança
+    @Bean//se esquecer a anotação não funciona
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+
+    }
 }
