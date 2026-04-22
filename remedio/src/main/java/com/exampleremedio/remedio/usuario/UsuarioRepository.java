9ccf7eb26c21e3bc53c4e76bbb8c756f014eb803
oliveirabilu <carloscesaroliveira13@hotmail.com>
2026-04-22 11:13:54 -0300

    feature: Criado uma interface repositorio Usuario, mapeando a (Entity e Id)

---
+++ remedio/src/main/java/com/exampleremedio/remedio/usuario/UsuarioRepository.java
@@ -0,0 +1,6 @@
+package com.exampleremedio.remedio.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
