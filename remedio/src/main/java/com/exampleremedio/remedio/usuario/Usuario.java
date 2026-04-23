cd50ff5aaa2926f4726f28b2520a6b744c1b1676
oliveirabilu <carloscesaroliveira13@hotmail.com>
2026-04-23 11:07:57 -0300

    feature: Para que o spring identifique os campos do usuario, temos que implementar uma interface do spring UserDetails, que virá com metodos prontos para implementar, booleanos e o controle de acesso, usamos o ROLE_USER para simplificar o exemplo

--- remedio/src/main/java/com/exampleremedio/remedio/usuario/Usuario.java
+++ remedio/src/main/java/com/exampleremedio/remedio/usuario/Usuario.java
@@ -1,10 +1,16 @@
 package com.exampleremedio.remedio.usuario;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_usuarios")
-public class Usuario {
+public class Usuario implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -37,5 +43,40 @@ public class Usuario {
     }
     public void setSenha(String senha) {
         this.senha = senha;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
