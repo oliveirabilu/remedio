b45f8a4fd5d6f8955f53b31b9d6fc2429c8358b4
oliveirabilu <carloscesaroliveira13@hotmail.com>
2026-04-23 16:04:05 -0300

    feature:Criado classe Service para Geração de Token,

---
+++ remedio/src/main/java/com/exampleremedio/remedio/usuario/TokenService.java
@@ -0,0 +1,30 @@
+package com.exampleremedio.remedio.usuario;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+
+@Service
+public class TokenService {
+    public String gerarToken(Usuario usuario) {
+        try {
+            var algorithm = Algorithm.HMAC256("123456");
+            return JWT.create()
+                    .withIssuer("remedio")
+                    .withSubject(usuario.getLogin())
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception) {
+           throw  new RuntimeException("Erro ao gerar o Token", exception);
+        }
+    }
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("03:00"));
+    }
+}
