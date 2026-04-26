87d9736c08388e4ec6b64f4c13504e08e7d4fb4d
oliveirabilu <carloscesaroliveira13@hotmail.com>
2026-04-26 10:46:34 -0300

    feature: Feito um metodo para verificar o token a cada solicitação, utilizado a biblioteca do JWT.io para auxiliar só modificado para sua aplicação, veio como try catch

--- remedio/src/main/java/com/exampleremedio/remedio/usuario/TokenService.java
+++ remedio/src/main/java/com/exampleremedio/remedio/usuario/TokenService.java
@@ -3,6 +3,8 @@ package com.exampleremedio.remedio.usuario;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -25,6 +27,19 @@ public class TokenService {
                     .sign(algorithm);
         } catch (JWTCreationException exception) {
            throw  new RuntimeException("Erro ao gerar o Token", exception);
+        }
+    }
+    public String getSubject(String TokenJwt){
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("remedio")
+                    .build()
+                    .verify(TokenJwt)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception){
+            throw  new RuntimeException("Token iválido ou expirado!");
         }
     }
     private Instant dataExpiracao() {
