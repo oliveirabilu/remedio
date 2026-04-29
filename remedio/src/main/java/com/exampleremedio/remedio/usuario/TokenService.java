def7096b6de356d7264ceb692ee63cc4bd56757e
oliveirabilu <carloscesaroliveira13@hotmail.com>
2026-04-29 20:44:29 -0300

    feature: Feito alterações atualizada no metodo gerar token, buscando login

--- remedio/src/main/java/com/exampleremedio/remedio/usuario/TokenService.java
+++ remedio/src/main/java/com/exampleremedio/remedio/usuario/TokenService.java
@@ -4,31 +4,32 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
 
 @Service
 public class TokenService {
     @Value("${api.security.token.secret}")
     private String secret;
-    public String gerarToken(Usuario usuario) {
+
+
+    public String gerarToken(String login) {
         try {
             var algorithm = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("remedio")
-                    .withSubject(usuario.getLogin())
+                    .withSubject(login)
                     .withExpiresAt(dataExpiracao())
                     .sign(algorithm);
         } catch (JWTCreationException exception) {
-           throw  new RuntimeException("Erro ao gerar o Token", exception);
+            throw new RuntimeException("Erro ao gerar o Token", exception);
         }
     }
+
     public String getSubject(String TokenJwt){
         try {
             var algorithm = Algorithm.HMAC256(secret);
@@ -42,7 +43,7 @@ public class TokenService {
             throw  new RuntimeException("Token iválido ou expirado!");
         }
     }
-    private Instant dataExpiracao() {
-        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("03:00"));
+      private Instant dataExpiracao() {
+        return Instant.now().plusSeconds(7200); // 2 horas
     }
 }
