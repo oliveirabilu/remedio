99a8029ca68297ea5ac04fc77b14c2a2f1e72144
oliveirabilu <carloscesaroliveira13@hotmail.com>
2026-04-29 20:30:09 -0300

    feature: Atualização para busca do usuario como retorno

--- remedio/src/main/java/com/exampleremedio/remedio/usuario/AuthenticationService.java
+++ remedio/src/main/java/com/exampleremedio/remedio/usuario/AuthenticationService.java
@@ -3,19 +3,36 @@ package com.exampleremedio.remedio.usuario;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationService implements UserDetailsService{
     private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthenticationService(UsuarioRepository usuarioRepository) {
+    public AuthenticationService(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
         this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = passwordEncoder;
     }
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        //não pode retornar null, então usaremos o repository
-        return usuarioRepository.findByLogin(username);
+        Usuario user = usuarioRepository.findByLogin(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("Usuário não encontrado");
+        }
+
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(user.getLogin())
+                .password(user.getSenha())
+                .roles("USER")
+                .build();
     }
+    public Usuario salvar(Usuario user) {
+        user.setSenha(passwordEncoder.encode(user.getSenha()));
+        return usuarioRepository.save(user);
+    }
+
 }
